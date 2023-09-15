814695d8570643c2262edabc79192ede636d598d
aslee <a.leepantoja@mail.utoronto.ca>
2023-09-15 16:59:14 -0400

    Fixed bug in Multiples.java

--- src/Multiples.java
+++ src/Multiples.java
@@ -6,7 +6,7 @@ public class Multiples {
 
         int multiples = 0; //counter
 
-        for (int i = 0; i < 1000; i++){
+        for (int i = 1; i < 1000; i++){
 
             if (i % 3 == 0 || i % 5 == 0) {
                multiples += 1;
