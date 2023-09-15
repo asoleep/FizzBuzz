efc1508242565cb1a1605fbe1c0c7c31b609a394
aslee <a.leepantoja@mail.utoronto.ca>
2023-09-15 01:53:57 -0400

    Modified Multiples.java

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,12 +1,21 @@
-public class Multiples {
+/**
+ * Prints how many multiples of 3 or 5 there are below 1000
+ */public class Multiples {
     public static void main(String[] args) {
+
+        int multiples = 0; //counter
+
         for (int i = 0; i < 1000; i++){
+
             if (i % 3 == 0) {
-                System.out.println(i);
+               multiples += 1;
             }
+
             else if (i % 5 == 0){
-                System.out.println(i);
+                multiples +=1;
             }
         }
+
+        System.out.println("There are " + multiples + " non-negative multiples of 3 or 5 below 1000.");
     }
 }
